aa71a6d8a4e58d9b0292aed10c041bfedfec46b3
chenzhiming-iri <chenzhiming-iri@360.cn>
2018-04-04 16:28:51 +0800

    test
    test

---
+++ testFramework/testFramework.h
@@ -0,0 +1,15 @@
+//
+//  testFramework.h
+//  testFramework
+//
+//  Created by chenzhiming on 2018/4/4.
+//  Copyright © 2018年 cm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testFramework : NSObject
+
+- (void)testFrameworkPrint:(NSString *)str;
+
+@end
---
+++ testFramework/testFramework.m
@@ -0,0 +1,18 @@
+//
+//  testFramework.m
+//  testFramework
+//
+//  Created by chenzhiming on 2018/4/4.
+//  Copyright © 2018年 cm. All rights reserved.
+//
+
+#import "testFramework.h"
+
+@implementation testFramework
+
+- (void)testFrameworkPrint:(NSString *)str
+{
+    NSLog(@"%@",str);
+}
+
+@end
